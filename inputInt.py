b60189f453bbb549524a5234e179d1c4f5f0c49c
spiderLambo <spiderLambo.dev@outlook.com>
2023-09-09 23:26:31 +0200

    update nb de lignes

--- inputInt.py
+++ inputInt.py
@@ -1,24 +1,42 @@
-def inputInt(a, b):
+def input_int(question, nombre_de_lignes=2):
     p = True
+    b = True
     while p:
-        print(b)
-        a = input()
+        while b:
+            if nombre_de_lignes == 1:
+                a = input(question)
+                b = False
+            elif nombre_de_lignes == 2:
+                print(question)
+                a = input()
+                b = False
+            else:
+                print("Le deusieme parametre est invalide")
         try:
             int(a)
             p = False
         except:
-            print("choisir un nombre entier")
+            print("Choisir un nombre entier")
     return int(a)
 
 
-def inputFloat(a, b):
+def input_float(question, nombre_de_lignes=2):
     p = True
+    b = True
     while p:
-        print(b)
-        a = input()
+        while b:
+            if nombre_de_lignes == 1:
+                a = input(question)
+                b = False
+            elif nombre_de_lignes == 2:
+                print(question)
+                a = input()
+                b = False
+            else:
+                print("Le deusieme parametre est invalide")
         try:
             float(a)
             p = False
         except:
-            print("choisir un nombre")
+            print("Choisir un nombre entier")
     return float(a)
